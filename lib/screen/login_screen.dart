7b2dffccc68c7a106ed2c0b40c491f55df8064f9
iswandi2002 <iswandiis058@gmail.com>
2024-07-12 01:41:07 +0700

    add login functionality

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -2,12 +2,77 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:presensi_app/screen/dashboard_screen.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class screenlogin extends StatelessWidget {
-  const screenlogin({Key? key}) : super(key: key);
+
+class LoginScreen extends StatefulWidget{
+  const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<LoginScreen> createState(){
+    return _LoginScreen();
+  }
+}
+
+
+class _LoginScreen extends State<LoginScreen> {
+
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isLoading = false;
+
+  Future<void> _login() async {
+    final username = _usernameController.text;
+    final password = _passwordController.text;
+
+    if (username.isEmpty || password.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please enter your username and password')),
+      );
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    const url = 'https://presensi.spilme.id/login'; // Replace with your server address
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'username': username,
+        'password': password,
+      }),
+    );
+    if (response.statusCode == 200) {
+      final responseBody = jsonDecode(response.body);
+      final token = responseBody['token'];
+
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('jwt', token);
+
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (context) => const DashboardScreen()),
+        (route) => false,
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Invalid username or password')),
+      );
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
@@ -15,169 +80,179 @@ class screenlogin extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 24),
           child: Column(
             children: [
-              const SizedBox(height: 80),
+              const SizedBox(height: 80,),
               Center(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    // Logo aplikasi
-                    Image.asset(
-                      'assets/images/logo.png',
-                      height: 128,
-                    ),
-                    const SizedBox(height: 10),
-                    RichText(
-                      textAlign: TextAlign.center,
-                      text: TextSpan(
-                        text: 'Selamat Datang\ndi ',
-                        style: GoogleFonts.manrope(
-                          fontSize: 32,
-                          color: const Color(0xFF101317),
-                          fontWeight: FontWeight.w800,
-                        ),
-                        children: const [
-                          TextSpan(
-                            text: 'PresensiApp',
-                            style: TextStyle(
-                                color: Color(0xFF12A3DA),
-                                fontWeight: FontWeight.w800),
-                          )
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 5),
-                    Text(
-                      'Halo, silahkan masuk untuk melanjutkan',
+                  //Logo aplikasi
+                  Image.asset(
+                    'assets/images/logo_polbeng.png',
+                    height: 128,
+                  ),
+                  const SizedBox(height: 10,),
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(
+                      text:'Selamat Datang\ndi ',
                       style: GoogleFonts.manrope(
-                        fontSize: 14,
-                        color: const Color(0xFFACAFB5),
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    const TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Username',
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          borderSide: BorderSide(color: Color(0xFF12A3DA)),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          borderSide: BorderSide(color: Color(0xFF12A3DA)),
-                        ),
-                      ),
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                    const SizedBox(height: 24),
-                    // Password TextField
-                    const TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          borderSide: BorderSide(color: Color(0xFF12A3DA)),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                          borderSide: BorderSide(color: Color(0xFF12A3DA)),
-                        ),
-                        suffixIcon: Icon(Icons.visibility_off),
-                      ),
-                      obscureText: true,
-                    ),
-                    const SizedBox(height: 8),
-                    // Forgot password
-                    GestureDetector(
-                      onTap: () {
-                        if (kDebugMode) {
-                          print('lupa password tapped');
-                        }
-                      },
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          'Lupa Password?',
-                          style: GoogleFonts.manrope(
-                            fontSize: 14,
-                            color: const Color(0xFF9B59B6),
-                          ),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 24),
-                    // Login Button
-                    ElevatedButton(
-                      onPressed: () {
-                        // Tambahkan logika autentikasi di sini
-                        // Misalnya, navigasi ke halaman dashboard setelah berhasil login
-                        Navigator.pushReplacementNamed(context, '/dashboard');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        minimumSize: const Size(double.infinity, 50), // width and height
-                        backgroundColor: const Color(0xFF12A3DA),
-                        foregroundColor: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                      child: Text(
-                        'Masuk',
-                        style: GoogleFonts.manrope(
-                          fontSize: 20,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      'Masuk dengan sidik jari?',
-                      style: GoogleFonts.manrope(
-                        fontSize: 14,
+                        fontSize: 32,
                         color: const Color(0xFF101317),
+                        fontWeight: FontWeight.w800,
+                      ),
+                      children: const[
+                        TextSpan(
+                          text: 'PresensiApp',
+                          style: TextStyle(
+                            color: Color(0xFF12A3DA),
+                            fontWeight: FontWeight.w800
+                          )
+                        )
+                      ]
+                    ),
+                  ),
+                  const SizedBox(height: 5,),
+                  Text(
+                    'Halo, silahkan masuk untuk melanjutkan',
+                    style: GoogleFonts.manrope(
+                      fontSize: 14,
+                      color: const Color(0xFFACAFB5),
+                    ),
+                  ),
+                  const SizedBox(height: 20,),
+              
+                  TextField(
+                    controller: _usernameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Username',
+                      enabledBorder:OutlineInputBorder(
+                        borderRadius:BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(color: Color(0xFF12A3DA)),
+                      ),
+                      focusedBorder:OutlineInputBorder(
+                        borderRadius:BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(color: Color(0xFF12A3DA)),
                       ),
                     ),
-                    TextButton(
-                      onPressed: () {
-                        // Fingerprint Tap
-                      },
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: const [
-                          Icon(Icons.fingerprint, size: 48, color: Colors.grey),
-                        ],
+                   
+                    //keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(height: 24),
+                  // Password TextField
+                  
+                   TextField(
+                    controller: _passwordController,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      enabledBorder:OutlineInputBorder(
+                        borderRadius:BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(color: Color(0xFF12A3DA)),
                       ),
+                      focusedBorder:OutlineInputBorder(
+                        borderRadius:BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(color: Color(0xFF12A3DA)),
+                      ),
+                      suffixIcon: Icon(Icons.visibility_off),
                     ),
-                    const SizedBox(height: 40),
-                    // Register New Account
-                    GestureDetector(
-                      onTap: () {
-                        // Register Tap
-                      },
-                      child: RichText(
-                        text: TextSpan(
-                          style: GoogleFonts.manrope(
-                            fontSize: 14,
-                            color: const Color(0xFF101317),
-                          ),
-                          children: const [
-                            TextSpan(text: 'Belum punya akun? '),
-                            TextSpan(
-                              text: 'Daftar',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xFF9B59B6),
-                              ),
-                            ),
-                          ],
+                    obscureText: true,
+                  ),
+                  const SizedBox(height: 8),
+                  //forgot password
+                  GestureDetector(
+                    onTap: (){
+                      if (kDebugMode) {
+                        print('lupa password tapped');
+                      }
+                    },
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        'Lupa Password?',
+                        style: GoogleFonts.manrope(
+                          fontSize: 14,
+                          color: const Color(0xFF9B59B6),
                         ),
                       ),
                     ),
+                  ),  
+                  const SizedBox(height: 24),
+                  // Login Button
+               
+                    _isLoading 
+                    ? const CircularProgressIndicator()
+                  // Login Button
+                  : ElevatedButton(
+                    onPressed: _login,
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.infinity, 50), // width and height
+                      backgroundColor: const Color(0xFF12A3DA),
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      )
+                    ),
+                    child: Text(
+                      'Masuk',
+                      style: GoogleFonts.manrope(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Text (
+                    'Masuk dengan sidik jari?',
+                    style: GoogleFonts.manrope(
+                      fontSize: 14,
+                      color: const Color(0xFF101317),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // Fingerprint Tap
+                    },
+                    child: const Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          Icons.fingerprint, 
+                          size: 48,
+                          color: Colors.grey
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  // Register New Account
+                  GestureDetector(
+                    onTap: () {
+                      // Register Tap
+                    },
+                    child: RichText(
+                      text: TextSpan(
+                        style: GoogleFonts.manrope(
+                          fontSize: 14,
+                          color: const Color(0xFF101317),
+                        ),
+                        children: const [
+                          TextSpan(text: 'Belum punya akun? '),
+                          TextSpan(
+                            text: 'Daftar',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Color(0xFF9B59B6),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                   ],
                 ),
               )
             ],
           ),
-        ),
-      ),
+        )
+      )
     );
   }
 }
